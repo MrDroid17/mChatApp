65c806842ccf859f9b367ddc44ba7606a3a586c9
Sobhit Kumar <sobhit2010@gmail.com>
2018-02-02 18:43:54 +0530

    Add progress Bar to activity_main.xml

--- app/src/main/java/com/kumar/mrdroid/chatapp/MessageAdapter.java
+++ app/src/main/java/com/kumar/mrdroid/chatapp/MessageAdapter.java
@@ -1,8 +1,26 @@
 package com.kumar.mrdroid.chatapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 /**
  * Created by mrdroid on 2/2/18.
  */
 
-public class MessageAdapter {
+public class MessageAdapter extends ArrayAdapter<Message> {
+    public MessageAdapter(@NonNull Context context, int resource, @NonNull List<Message> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
