699b05d0e56288d52732552549d1d7b4e035b198
Sobhit Kumar <sobhit2010@gmail.com>
2018-02-02 21:54:57 +0530

    Add MessageAdapter Class which extends ArrayAdapter

--- app/src/main/java/com/kumar/mrdroid/chatapp/MessageAdapter.java
+++ app/src/main/java/com/kumar/mrdroid/chatapp/MessageAdapter.java
@@ -1,11 +1,17 @@
 package com.kumar.mrdroid.chatapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -21,6 +27,42 @@ public class MessageAdapter extends ArrayAdapter<Message> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        if(convertView == null){
+            convertView = ((Activity) getContext()).getLayoutInflater()
+                    .inflate(R.layout.item_message, parent, false);
+        }
+
+        ImageView mImage = convertView.findViewById(R.id.photoImageView);
+        TextView mMessage = convertView.findViewById(R.id.messageTextView);
+        TextView mSenderName = convertView.findViewById(R.id.nameTextView);
+
+        // get position of message
+        Message messageObject  = getItem(position);
+        boolean hasImage = messageObject.getmPhotoUrl() != null;
+
+        /***
+         *    If  Image is send as the message
+         */
+        if(hasImage){
+            mMessage.setVisibility(View.GONE);
+            mImage.setVisibility(View.VISIBLE);
+
+            //render image using glide library in mImage
+            Glide.with(mImage.getContext()).
+                    load(messageObject.getmPhotoUrl()).
+                    into(mImage);
+        }else{
+            /***
+             *    If  text is send as the message
+             */
+            mMessage.setVisibility(View.VISIBLE);
+            mImage.setVisibility(View.GONE);
+            mMessage.setText(messageObject.getmMessage());
+        }
+
+        mSenderName.setText(messageObject.getmSenderName());
+
+        return convertView;
     }
+
 }
